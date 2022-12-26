85a2fe70a702f094b3641ad43f09c393bddb21b5
davide <79225175+BavidDev@users.noreply.github.com>
2022-12-26 22:12:54 +0100

    Update object.py

--- object.py
+++ object.py
@@ -13,14 +13,33 @@ idTable = [
         1.67 * p(10, -27),
         (  ##BDE: H-ID
             436,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
             428,
-        )
+        ),
+        ()
     ),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
     (
         (6.6 * p(10,-11), 6.6 * p(10,-11)),
         2.66 * p(10,-26),
         (
             428,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
             498,
         ),
 
@@ -30,68 +49,20 @@ idTable = [
 class EConfig:
 
     def distribute(self, e):
-        Kmax = 2
-        Lmax = 8
-        Mmax = 18
-        Nmax = 32
-        Omax = 50
-        Pmax = 72
 
-        for i in range(1,e):
-            if e > Kmax:
-                self.K = Kmax
-                e-=2
-
-                if e > Lmax:
-                    self.L = Lmax
-                    e-=Lmax
-
-                    if e > Mmax:
-                        self.M = Mmax
-                        e-=Mmax
-
-                        if e > Nmax:
-                            self.N = Nmax
-                            e-=Nmax
-
-                            if e > Omax:
-                                self.O = Omax
-                                e-=Omax
-
-                                if e > Pmax:
-                                    self.P = Pmax
-                                    e-=Pmax
-                                else:
-                                    self.P = e
-                                    return
-
-                            else:
-                                self.O = e
-                                return
-
-                        else:
-                            self.N = e
-                            return
-
-                    else:
-                        self.M = e
-                        return
-
-                else:
-                    self.L = e
-                    return
-
+        for i in range(0,5):
+            if e > self.maxes[i]:
+                self.Shells[i] = self.maxes[i]
+                e-=self.maxes[i]
             else:
-                self.K = e
+                self.Shells[i] = e
                 return
 
     def __init__(self, e):
-        self.K = 0
-        self.L = 0
-        self.M = 0
-        self.N = 0
-        self.O = 0
-        self.P = 0
+        self.Shells = [0,0,0,0,0,0]
+        self.maxes = [2,8,18,32,50,72]
+
+        self.BondCount = 0
 
         self.distribute(e)
 
@@ -103,44 +74,23 @@ class EConfig:
         Omax = 50
         Pmax = 72
 
-        if self.K != Kmax:
-            if self.K == 0:
-                return "K"
-            return "K"
+        for i in range(0,5):
+            if self.Shells[i] != self.maxes[i]:
+                if self.Shells[i] == 0:
+                    if i==0: return i
 
-        if self.L != Lmax:
-            if self.L == 0:
-                return "K"
-            return "L"
+                    return i-1
+                return i
 
-        if self.M != Mmax:
-            if self.M == 0:
-                return "L"
-            return "M"
+    def excite(self):
 
-        if self.N != Nmax:
-            if self.M == 0:
-                return "M"
-            return "N"
+        outerShell = self.outerShell()
+        self.Shells[outerShell] -= 1
+        self[outerShell+1] += 1
 
-        if self.O != Omax:
-            if self.M == 0:
-                return "M"
-            return "O"
-
-        if self.P != Pmax:
-            if self.M == 0:
-                return "O"
-            return "P"
-
-        def excite():
-
-            LToN = {"K":1,"L":2,"M":3,"N":4,"O":5,"P":6}
-            NToL = {1:"K",2:"L",3:"M",4:"N",5:"O",6:"P"}
-
-            outerShell = self.outerShell()
-            self[outerShell] -= 1
-            self[NToL[LToN[outerShell]+1]] += 1
+    def valence(self):
+        outerShell = self.outerShell()
+        return self.maxes[outerShell] - (self.Shells[outerShell] + self.BondCount)
 
 class object:
     def __init__(self, id, position, velocity):
@@ -151,5 +101,5 @@ class object:
         self.m = idTable[id][1]
         self.v = velocity
         self.c = 0*chargeConstant
-        self.BondCount = 0
         self.EConfig = EConfig(id+1)
+        self.EConfig.distribute(id+1)
